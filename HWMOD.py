227bc9218fa68123eb478cbfff935b582e7d27a9
N0l0z <147103853+N0l0z@users.noreply.github.com>
2023-11-30 02:17:09 -0600

    first commit

---
+++ HWMOD.py
@@ -0,0 +1,16 @@
+# Module to be imported into Jupyter Notebook
+def Square_Footage():
+    width = float(input())
+    length = float(input())
+    area = width * length
+    print("area of square footage is", area)
+    
+#circumference= {width }
+
+def Circum_Ference():
+    diameter = float(input())
+    circlefootage = (diameter)*(3.14)
+print("area of circle is", circlefootage)
+
+
+
